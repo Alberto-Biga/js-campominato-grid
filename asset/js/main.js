6842080f8d0fed5022a43a46ecb81a77f181f51a
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-21 16:01:09 +0100

    creazione griglia in base a scelta utente

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,6 +4,32 @@ let btnPlay= document.getElementById(`play`);
 btnPlay.addEventListener(`click`,function(){
     //ottengo e salvo la difficoltà selezionata
     let sceltaUtente = document.getElementById(`difficulty`).value;
-    console.log(`la difficoltà è la numero ${sceltaUtente}`);
+    console.log(`la difficoltà è la numero ${sceltaUtente} `);
+    //stampare griglia come richiesto da utente. Ho 3 opzioni
+    //griglia 10x10
+    if( sceltaUtente == "1"){
+        for (let i = 1; i<= 100; i++){
+            let cella = document.createElement(`div`);
+            cella.classList.add(`box10`);
+            grid.appendChild(cella)
+        }
+    //griglia 9x9
+    }else if( sceltaUtente == "2"){
+        for (let i = 1; i<= 81; i++){
+            let cella = document.createElement(`div`);
+            cella.classList.add(`box9`);
+            grid.appendChild(cella)
+        }
+    //griglia 7x7
+    }else if( sceltaUtente == "3"){
+        for (let i = 1; i<= 49; i++){
+            let cella = document.createElement(`div`);
+            cella.classList.add(`box7`);
+            grid.appendChild(cella)
+        }
+    }
+
+
+
 })
 
