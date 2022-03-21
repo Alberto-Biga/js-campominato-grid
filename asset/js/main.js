528fe4138df98d8201b48cd66842b5911e9a3ca0
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-21 17:39:18 +0100

    aggiustamenti

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,54 +7,33 @@ btnPlay.addEventListener(`click`,function(){
     console.log(`la difficoltà è la numero ${sceltaUtente} `);
     //stampare griglia come richiesto da utente. Ho 3 opzioni
     //griglia 10x10
+    let numCelle;
     if( sceltaUtente == "1"){
-        for (let i = 1; i<= 100; i++){
-            //creo tutte le celle
-            let cella = document.createElement(`div`);
-            //aggiungo numeri alle celle
-            cella.innerHTML += i
-            //aggiungo classi alle celle
-            cella.classList.add(`box10`);
-            grid.appendChild(cella)
-            //modifico colore celle al click
-            cella.addEventListener(`click`,function (){
-                console.log(this)
-                this.classList.add(`boxCliccato`)
-            })
-        }
-    //griglia 9x9
+        numCelle = 100
     }else if( sceltaUtente == "2"){
-        for (let i = 1; i<= 81; i++){
-            //creo tutte le celle
-            let cella = document.createElement(`div`);
-            //aggiungo numeri alle celle
-            cella.innerHTML += i
-            //aggiungo classi alle celle
-            cella.classList.add(`box9`);
-            grid.appendChild(cella)
-            //modifico colore celle al click
-            cella.addEventListener(`click`,function (){
-                console.log(this)
-                this.classList.add(`boxCliccato`)
-            })
-        }
-    //griglia 7x7
+        numCelle = 81;
     }else if( sceltaUtente == "3"){
-        for (let i = 1; i<= 49; i++){
-            //creo tutte le celle
-            let cella = document.createElement(`div`);
-            //aggiungo numeri alle celle
-            cella.innerHTML += i
-            //aggiungo classi alle celle
-            cella.classList.add(`box7`);
-            grid.appendChild(cella)
-            //modifico colore celle al click
-            cella.addEventListener(`click`,function (){
-                console.log(this)
-                this.classList.add(`boxCliccato`)
-            })
-        }
+        numCelle = 49;
     }
-    
+    for (let i = 1; i<= numCelle; i++){
+        //creo tutte le celle
+        let cella = document.createElement(`div`);
+        //aggiungo numeri alle celle
+        cella.innerHTML += i
+        //aggiungo classi alle celle
+        if(numCelle == 100){
+            cella.classList.add(`box10`);
+        }else if(numCelle == 81){           
+            cella.classList.add(`box9`);
+        }else if(numCelle == 49){           
+            cella.classList.add(`box7`);
+        }
+        
+        grid.appendChild(cella)
+        //modifico colore celle al click
+        cella.addEventListener(`click`,function (){
+            console.log(this)
+            this.classList.add(`boxCliccato`)
+        })
+    }   
 })
-
