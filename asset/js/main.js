91e3de1cc2a5905814002fe7b6f6819cd4b3154c
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-22 11:40:48 +0100

    FUNZIONE NUMERO RANDOM DA AGGIUSTARE

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,5 @@
+//array nutrito da funzione numero random
+let array = [];
 //funzione per stampa griglia
 let btnPlay= document.getElementById(`play`);
 //al click del bottone play parte
@@ -14,12 +16,15 @@ btnPlay.addEventListener(`click`,function(){
         numCelle = 81;
     }else if( sceltaUtente == "3"){
         numCelle = 49;
-    }
+    } 
+    // se premo play pulisco griglia o ne creo un'altra
+    grid.innerHTML = "";   
     //creo tutte le celle
     for (let i = 1; i<= numCelle; i++){        
         let cella = document.createElement(`div`);
-        //aggiungo numeri alle celle
-        cella.innerHTML += i
+        grid.appendChild(cella);
+        //aggiungo numeri casuali da 1 a 100 alle celle (no doppioni)
+        cella.innerHTML += array[i];
         //aggiungo classi alle celle
         if(numCelle == 100){
             cella.classList.add(`box10`);
@@ -27,13 +32,26 @@ btnPlay.addEventListener(`click`,function(){
             cella.classList.add(`box9`);
         }else if(numCelle == 49){           
             cella.classList.add(`box7`);
-        }
-        
-        grid.appendChild(cella)
+        }          
         //modifico colore celle al click
         cella.addEventListener(`click`,function (){
             console.log(this)
             this.classList.add(`boxCliccato`)
         })
-    }   
-})
+    }       
+});
+//funzione per generare numero random tra 1 e 100 
+//DA RISOLVERE DUPLICATI
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+};    
+
+for(y = 1; y <= 100; y++){   
+    let numeroRandom = getRandomInt(1, 100);
+    do{
+        array.push(numeroRandom)
+    } while (!array.includes(numeroRandom));
+}
+console.log(array);
